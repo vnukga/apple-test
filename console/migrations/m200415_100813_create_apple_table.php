273da5fe2fa895434ee69dd3f29c6aa75489c7ba
Denis Korablev <d.korablev.dev@gmail.com>
2020-04-15 13:42:08 +0300

    MIGRATION: apple - миграция для таблицы.

---
+++ console/migrations/m200415_100813_create_apple_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%apple}}`.
+ */
+class m200415_100813_create_apple_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%apple}}', [
+            'id' => $this->primaryKey(),
+            'color' => $this->string(7)->notNull(),
+            'appeared_at' => $this->integer(11)->notNull(),
+            'falled_at' => $this->integer(11),
+            'eaten' => $this->integer(3)->defaultValue(0),
+            'status' => $this->integer(2)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%apple}}');
+    }
+}
